34766e6b12e2d87033a38271dfba311615e61738
Christian Parker <christianparker512@gmail.com>
2021-09-09 07:14:56 -0500

    dog inheriting animal characteristics

--- src/parker/learningJava/Dog.java
+++ src/parker/learningJava/Dog.java
@@ -1,6 +1,13 @@
 package parker.learningJava;
 
 public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private int weight;
+
     public Dog(String name, int brain, int body, int size, int weight) {
         super(name, brain, body, size, weight);
     }
